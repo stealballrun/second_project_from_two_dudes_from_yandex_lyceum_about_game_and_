038ff9a2778350b757221962b5bf8aa439334b0c
bivstrog <43310038+bivstrog@users.noreply.github.com>
2019-01-22 20:31:15 +0300

    debug v.0.0.0.1

--- Pawn.py
+++ Pawn.py
@@ -1,3 +1,51 @@
+one = [[None, None, None, None, None, None],
+       [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), None],
+       [(1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), None],
+       [(2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7), None],
+       [(3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (3, 7), (3, 8), None],
+       [(4, 0), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7), (4, 8), (4, 9), None],
+       [(5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8), (5, 9), (5, 10)],
+       [(6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8), (6, 9)],
+       [(7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7), (7, 8)],
+       [(8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6), (8, 7)],
+       [(9, 1), (9, 2), (9, 3), (9, 4), (9, 5), (9, 6)]]
+
+two = [[(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6)],
+       [(2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7)],
+       [(3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (3, 7), (3, 8)],
+       [(4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7), (4, 8), (4, 9)],
+       [(5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8), (5, 9), (5, 10)],
+       [(6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8), (6, 9), None],
+       [(7, 0), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7), (7, 8), None],
+       [(8, 0), (8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6), (8, 7), None],
+       [(9, 0), (9, 1), (9, 2), (9, 3), (9, 4), (9, 5), (9, 6), None],
+       [(10, 0), (10, 1), (10, 2), (10, 3), (10, 4), (10, 5), None],
+       [None, None, None, None, None, None]]
+
+three = [[(1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5)],
+         [(2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6)],
+         [(3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (3, 7)],
+         [(4, 0), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7), (4, 8)],
+         [(5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8), (5, 9)],
+         [None, (6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8), (6, 9)],
+         [None, (7, 0), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7), (7, 8)],
+         [None, (8, 0), (8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6), (8, 7)],
+         [None, (9, 0), (9, 1), (9, 2), (9, 3), (9, 4), (9, 5), (9, 6)],
+         [None, (10, 0), (10, 1), (10, 2), (10, 3), (10, 4), (10, 5)],
+         [None, None, None, None, None, None]]
+
+four = [[None, None, None, None, None, None],
+        [None, (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5)],
+        [None, (1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6)],
+        [None, (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7)],
+        [None, (3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (3, 7), (3, 8)],
+        [None, (4, 0), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7), (4, 8), (4, 9)],
+        [(5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8), (5, 9)],
+        [(6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8)],
+        [(7, 0), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7)],
+        [(8, 0), (8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6)],
+        [(9, 0), (9, 1), (9, 2), (9, 3), (9, 4), (9, 5)]]
+
 field =  [[None, None, None, None, None, None],
           [None, None, None, None, None, None, None],
           [None, None, None, None, None, None, None, None],
@@ -9,15 +57,16 @@ field =  [[None, None, None, None, None, None],
           [None, None, None, None, None, None, None, None],
           [None, None, None, None, None, None, None],
           [None, None, None, None, None, None]]
-class Pawn: # ïåøêà
+
+class Pawn:
  
-    def __init__(self, row, col, color):
+    def __init__(self, col, row, color):
         self.row = row
         self.col = col
         self.color = color
         self.step = 0
  
-    def set_position(self, row, col):
+    def set_position(self, col, row):
         self.row = row
         self.col = col
  
@@ -27,33 +76,45 @@ class Pawn: # ïåøêà
     def get_color(self):
         return self.color
  
-    def can_move(self, row, col):
+    def can_move(self, col, row):
         if self.col != col:
             return False
  
-        # Ïåøêà ìîæåò ñäåëàòü èç íà÷àëüíîãî ïîëîæåíèÿ õîä íà 2 êëåòêè
-        # âïåð¸ä, ïîýòîìó ïîìåñòèì èíäåêñ íà÷àëüíîãî ðÿäà â start_row.
         if self.color == WHITE:
             direction = 1
         else:
             direction = -1
  
-        # õîä íà 1 êëåòêó
         if self.row + direction == row:
             return True
  
-        # õîä íà 2 êëåòêè èç íà÷àëüíîãî ïîëîæåíèÿ
-        if self.row == start_row and self.row + 2 * direction == row:
+        if self.step == 0 and self.row + 2 * direction == row:
             return True
- 
+        
         return False
-    def can_eat(self, row, col):
-        if self.row == row and (self.col + 1 == col or self.col - 1 == col) and direction == 1:
-            return True
-          if self.row - 1 == row and (self.col + 1 == col or self.col - 1 == col) and direction == -11:
-            return True
-        return False
-    def check_Q(self, row, col):
+    
+    def can_eat(self, col, row):
+        if direction == 1:
+            if (col, row) == one[self.col][self.row]:
+                return True
+            
+            elif (col, row) == two[self.col][self.row]:
+                return True
+            
+            else:
+                return False
+            
+        else:
+            if (col, row) == four[self.col][self.row]:
+                return True
+            
+            elif (col, row) == three[self.col][self.row]:
+                return True
+            
+            else:
+                return False            
+    
+    def check_Q(self, col, row):
         if direction == 1 and len(field[col]) == row + 1:
             return True
         if direction == -1 and row == 0:
