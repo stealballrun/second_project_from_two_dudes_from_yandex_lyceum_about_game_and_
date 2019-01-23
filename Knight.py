762d4104c922642fb01f9339d7239d45c363e029
bivstrog <43310038+bivstrog@users.noreply.github.com>
2019-01-23 18:11:11 +0300

    v.0.0.0.2

---
+++ Knight.py
@@ -0,0 +1,110 @@
+one = [[None, None, None, None, None, None],
+       [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), None],
+       [(1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), None],
+       [(2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7), None],
+       [(3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (3, 7), (3, 8), None],
+       [(4, 0), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7), (4, 8), (4, 9), None],
+       [(5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8), (5, 9), (5, 10)],
+       [(6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8), (6, 9)],
+       [(7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7), (7, 8)],
+       [(8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6), (8, 7)],
+       [(9, 1), (9, 2), (9, 3), (9, 4), (9, 5), (9, 6)]]
+
+two = [[(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6)],
+       [(2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7)],
+       [(3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (3, 7), (3, 8)],
+       [(4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7), (4, 8), (4, 9)],
+       [(5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8), (5, 9), (5, 10)],
+       [(6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8), (6, 9), None],
+       [(7, 0), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7), (7, 8), None],
+       [(8, 0), (8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6), (8, 7), None],
+       [(9, 0), (9, 1), (9, 2), (9, 3), (9, 4), (9, 5), (9, 6), None],
+       [(10, 0), (10, 1), (10, 2), (10, 3), (10, 4), (10, 5), None],
+       [None, None, None, None, None, None]]
+
+three = [[(1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5)],
+         [(2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6)],
+         [(3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (3, 7)],
+         [(4, 0), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7), (4, 8)],
+         [(5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8), (5, 9)],
+         [None, (6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8), (6, 9)],
+         [None, (7, 0), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7), (7, 8)],
+         [None, (8, 0), (8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6), (8, 7)],
+         [None, (9, 0), (9, 1), (9, 2), (9, 3), (9, 4), (9, 5), (9, 6)],
+         [None, (10, 0), (10, 1), (10, 2), (10, 3), (10, 4), (10, 5)],
+         [None, None, None, None, None, None]]
+
+four = [[None, None, None, None, None, None],
+        [None, (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5)],
+        [None, (1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6)],
+        [None, (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7)],
+        [None, (3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (3, 7), (3, 8)],
+        [None, (4, 0), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7), (4, 8), (4, 9)],
+        [(5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8), (5, 9)],
+        [(6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8)],
+        [(7, 0), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7)],
+        [(8, 0), (8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6)],
+        [(9, 0), (9, 1), (9, 2), (9, 3), (9, 4), (9, 5)]]
+
+field =  [[None, None, None, None, None, None],
+          [None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None, None],
+          [None, None, None, None, None, None, None],
+          [None, None, None, None, None, None]]
+
+class Knight:
+    def __init__(self, col, row, color, N):
+        self.color = color
+        self.col = col
+        self.N = N
+        self.color = color        
+        self.step = 0
+
+    def get_color(self):
+        return self.color
+
+    def char(self):
+        return 'N', self.N
+
+    def can_move(self, col, row):
+        c1, r1 = one[self.col][self.row]
+        c2, r2 = two[self.col][self.row]
+        c3, r3 = three[self.col][self.row]
+        c4, r4 = four[self.col][self.row]
+        if one[self.col + 2][self.row] == (col, row):
+            return True
+        
+        elif two[self.col + 2][self.row] == (col, row):
+            return True
+        
+        elif three[self.col - 2][self.row] == (col, row):
+            return True
+        
+        elif four[self.col - 2][self.row] == (col, row):
+            return True
+        
+        elif one[c1][r1] == (col, row - 1) or one[c1][r1] == two[col][row]:
+            return True
+        
+        elif two[c2][r2] == (col, row - 1) or two[c2][r2] == one[col][row]:
+            return True
+        elif three[c3][r3] == (col, row + 1) or three[c3][r3] == four[col][row]:
+            return True
+        
+        elif four[c4][r4] == (col, row + 1) or four[c4][r4] == three[col][row]:
+            return True
+        
+        else:
+            return False
+
+    def can_attack(self, col, row):
+        return self.can_move(self, col, row)
+    
+    def check_Q(self, col, row):
+        return False    
